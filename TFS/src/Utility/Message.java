5495c4662b18f0355b4e53ec0197088a0a92ed21
Handroo <hanandre@usc.edu>
2014-04-10 01:28:21 -0700

    added message class and chunkmetadata class. started test5 implementation

--- TFS/src/Utility/Message.java
+++ TFS/src/Utility/Message.java
@@ -1,8 +1,22 @@
 package Utility;
 
 import java.net.*;  
+import java.util.*;
 import java.io.*; 
 
 public class Message implements Serializable{
-
+	
+	public Message(){
+		
+	}
+	//This is for test5
+	public Message(String fp){
+		filePath = fp;
+	}
+//	List<Character> filePath = new ArrayList<Character>();
+	String filePath;
+	int startByte;
+	int byteLength;
+	ChunkMetadata chunkClass;
+	
 }
