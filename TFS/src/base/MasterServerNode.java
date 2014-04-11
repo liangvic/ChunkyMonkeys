33717582291a5c2b3c96d144c3082555745f1c0d
Jessica <wang336@usc.edu>
2014-04-11 05:17:23 -0700

    merging

--- TFS/src/base/MasterServerNode.java
+++ TFS/src/base/MasterServerNode.java
@@ -26,6 +26,7 @@ public class MasterServerNode extends ServerNode {
 	public MasterServerNode() {
 		LoadChunkServerMap();
 		LoadNamespaceMap();
+//		NamespaceMap.put("1", new NamespaceNode());
 	}
 
 	// Don't call on this for now; using monolith structure
@@ -81,9 +82,9 @@ public class MasterServerNode extends ServerNode {
 		} else if (inputMessage.type == msgType.DELETEDIRECTORY
 				&& inputMessage.sender == serverType.CHUNKSERVER) {
 			if (inputMessage.success == msgSuccess.REQUESTSUCCESS) {
-				//SendSuccessMessageToClient();
+				SendSuccessMessageToClient();
 			} else {
-				//SendErrorMessageToClient();
+				SendErrorMessageToClient();
 			}
 		}
 		else if(inputMessage.type == msgType.CREATEDIRECTORY) 
@@ -132,14 +133,16 @@ public class MasterServerNode extends ServerNode {
 
 	}
 
-	public void SendSuccessMessageToClient(Message successMessage) {
+	public void SendSuccessMessageToClient() {
+		Message successMessage = new Message(msgType.CREATEDIRECTORY);
 		successMessage.success = msgSuccess.REQUESTSUCCESS;
 		client.DealWithMessage(successMessage);
 	}
 
-	public void SendErrorMessageToClient(Message errorMessage) {
-		errorMessage.success = msgSuccess.REQUESTERROR;
-		client.DealWithMessage(errorMessage);
+	public void SendErrorMessageToClient() {
+		Message successMessage = new Message(msgType.CREATEDIRECTORY);
+		successMessage.success = msgSuccess.REQUESTERROR;
+		client.DealWithMessage(successMessage);
 	}
 
 	public void MDeleteDirectory(String filePath) {
@@ -243,7 +246,7 @@ public class MasterServerNode extends ServerNode {
 		//if folder doesn't exist or file already exists
 		if (NamespaceMap.get(filepath) == null || chunkServerMap.get(hashstring) != null){
 
-			SendErrorMessageToClient(new Message(msgType.CREATEFILE));
+			SendErrorMessageToClient();
 		} else {
 
 				String newName = filepath + "\\" + filename;
@@ -282,33 +285,37 @@ public class MasterServerNode extends ServerNode {
 
 	public void CreateDirectory(String filepath) {
 		if (!NamespaceMap.containsKey(filepath)) { // directory doesn't exist
-			File path = new File(filepath);
-			String parentPath = path.getParent();
-			String parent;
-			if(parentPath == null) {
-				parent = filepath;
-			}
-			else {
-				parent = parentPath;
-			}
-			if(!NamespaceMap.containsKey(parent) && !(parent.equals(filepath))) {
-				// parent directory does not exist
-				SendErrorMessageToClient(new Message(msgType.CREATEDIRECTORY, filepath));
-				return;
-			}
-			else if(NamespaceMap.containsKey(parent)) {
-				NamespaceMap.get(parent).children.add(filepath);
+			String delim = "\\+";
+			String[] tokens = filepath.split(delim);
+			
+			if (tokens.length >= 1)
+			{
+				String supposedParent = tokens[0];
+				if (tokens.length >= 2){
+					for (int i = 1; i < tokens.length - 1 ; i++){			
+						supposedParent += "\\" + tokens[i]; 
+					}
+				}				
+				if(NamespaceMap.containsKey(supposedParent)) 
+				{
+					
+					NamespaceMap.get(supposedParent).children.add(filepath);
+				}
+				else{
+					
+//					throw new Exception();
+				}
 			}
 
 			NamespaceNode newNode = new NamespaceNode();
 			NamespaceMap.put(filepath, newNode);
-			SendSuccessMessageToClient(new Message(msgType.CREATEDIRECTORY, filepath));
+			SendSuccessMessageToClient();
 			tfsLogger.LogMsg("Created directory " + filepath);
 			
 			WritePersistentNamespaceMap(filepath,newNode);
 		} else // directory already exists
 		{
-			SendErrorMessageToClient(new Message(msgType.CREATEDIRECTORY, filepath));
+			SendErrorMessageToClient();
 		}
 
 		/*
