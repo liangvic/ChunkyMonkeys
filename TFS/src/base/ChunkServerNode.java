dec294ef8b807353277aa42113b585d25f27626f
Jessica <wang336@usc.edu>
2014-04-11 02:15:32 -0700

    renamed dataStorage files and added load method to ChunkServerNode

--- TFS/src/base/ChunkServerNode.java
+++ TFS/src/base/ChunkServerNode.java
@@ -12,6 +12,7 @@ import java.util.Random;
 
 import Utility.ChunkMetadata;
 import Utility.Message;
+import Utility.chunkLocation;
 import Utility.Message.msgSuccess;
 import Utility.Message.msgType;
 
@@ -94,4 +95,56 @@ public class ChunkServerNode extends ServerNode{
     		master.DealWithMessage(successMessageToMaster);
     	}*/
     }
+    
+    public void LoadServerNodeMap()
+	{
+		try {
+			FileReader fr = new FileReader("dataStorage/SData_ChunkMap.txt");
+			BufferedReader textReader = new BufferedReader(fr);
+			String textLine;
+			
+			while((textLine = textReader.readLine())!= null)
+			{
+				//STRUCTURE///
+				//KEY VERSION# SIZEOF_LOCATIONLIST 
+				//CHUNKLOCATION1_IP CHUNKLOCATION1_PORT ... CHUNKLOCATIONN_IP CHUNKLOCATIONN_PORT
+				//CHUNKHASH
+				//REFERENCECOUNT
+				String[] data = textLine.split("\t");
+				
+				String key;
+				key = data[0];
+				
+				int version = Integer.parseInt(data[1]);
+				
+				List<chunkLocation> locations = new ArrayList<chunkLocation>();
+				int locationSize = locations.size();
+				
+				for(int i=3; i<3+(locationSize/2); i=i+2)
+				{
+					locations.add(new chunkLocation(data[i],Integer.parseInt(data[i+1])));
+				}
+				
+				List<Integer> hash = new ArrayList<Integer>();
+				String tempHash = data[3+(locationSize/2)];
+				for(int i=0;i<tempHash.length();i++)
+				{
+					hash.add(Character.getNumericValue(tempHash.charAt(i)));//adds at end
+				}
+				tempHash = hash.toString();
+				int count = Integer.parseInt(data[data.length-1]);
+				
+				ChunkMetadata newMetaData = new ChunkMetadata(version,locations,Integer.parseInt(tempHash),count);
+				
+				chunkMap.put(newMetaData.chunkHash, newMetaData);
+			}
+			textReader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
